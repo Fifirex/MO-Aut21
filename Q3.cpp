29cae3a7caa2967a3a174bb32f157a94ad2bf071
Diya Sarkar <62467890+Dabbu100@users.noreply.github.com>
2021-10-23 01:08:16 +0530

    Update Q3.cpp

--- Q3.cpp
+++ Q3.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main() {
+    double mass_flow = 2000.0/3600; //in kg/s
     double d_f = 0.025 * 1000; // in mm
     double d_p = 0.018 * 1000; // in mm
     int x  = 3; //Roll Number 20CH10016
@@ -13,16 +14,16 @@ int main() {
     double k_b; //Bond's constant
     // work_bond = k_b*((1/sqrt(d_p) - (1/sqrt(d_f)))
     double t = (1/sqrt(d_p)) - (1/sqrt(d_f));
-    k_b = initial_power/t;
+    k_b = initial_power/(mass_flow*t);
     double t1 = (1/sqrt(d_pnew)) - (1/sqrt(d_f));
-    double final_power =  (k_b)*t1;
+    double final_power =  (k_b)*t1*mass_flow;
     cout << "Ans (a): The power consumption is " << final_power << "Hp." << endl;
 
     // part (B) //
     double k_k; //Kick's constant
     // work_kick = k_k*ln(df/dp)
-    k_k = initial_power/(log(d_f/d_p));
-    final_power =  (k_b)*log(d_f/d_pnew);
+    k_k = initial_power/(mass_flow*log(d_f/d_p));
+    final_power =  (k_k)*log(d_f/d_pnew)*mass_flow;
     cout << "Ans (b): The power consumption using Kick's Law is " << final_power << "Hp." << endl;
     return 0;
 }
